b51b51d435e39ac4bcb9a6898029d29090cf8e90
Kama <56154113+kamillabadalova@users.noreply.github.com>
2019-12-16 23:52:17 +0300

    Create Table.java

---
+++ Table.java
@@ -0,0 +1,22 @@
+package Kursovaya;
+
+public class Table {
+    String year, num;
+
+    public Table(String year, String num) {
+        this.year = year;
+        this.num = num;
+    }
+    public String getYear() {
+        return year;
+    }
+    public void setYear(String year) {
+        this.year = year;
+    }
+    public String getNum() {
+        return num;
+    }
+    public void setNum(String num) {
+        this.num = num;
+    }
+}
