50bcf8728a3cfd94c8cd66e1bfc33d88bcf81ca9
Kama <56154113+kamillabadalova@users.noreply.github.com>
2019-12-16 23:38:17 +0300

    Update and rename README.md to Const.java

---
+++ Const.java
@@ -0,0 +1,11 @@
+package Kursovaya;
+
+public class Const {
+    public static final String USER_TABLE = "users" ;
+    public static final String USERS_NAME ="name";
+    public static final String USERS_SURNAME ="surname";
+    public static final String USERS_LOGIN ="login";
+    public static final String USERS_PASSWORD ="password";
+    public static final String USERS_LOCATION ="location";
+    public static final String USERS_MAIL="mail";
+}
