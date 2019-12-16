3043608bb75018463a38d5afbadad896dd7a62b7
Kama <56154113+kamillabadalova@users.noreply.github.com>
2019-12-16 23:38:59 +0300

    Update Const.java

--- Const.java
+++ Const.java
@@ -7,5 +7,5 @@ public class Const {
     public static final String USERS_LOGIN ="login";
     public static final String USERS_PASSWORD ="password";
     public static final String USERS_LOCATION ="location";
-    public static final String USERS_MAIL="mail";
+    public static final String USERS_MAIL="mail"; 
 }
